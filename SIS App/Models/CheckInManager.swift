110875d5a338275476d33683e477c6aaf969ef18
wangy <wangyunze412@gmail.com>
2020-11-07 11:38:30 +0800

    Added CheckInManager stubs

---
+++ SIS App/Models/CheckInManager.swift
@@ -0,0 +1,13 @@
+//
+//  CheckInManager.swift
+//  SIS App
+//
+//  Created by Wang Yunze on 7/11/20.
+//
+
+import Foundation
+
+struct CheckInManager {
+    static func checkIn(to: Room) {}
+    static func checkOut() {}
+}
