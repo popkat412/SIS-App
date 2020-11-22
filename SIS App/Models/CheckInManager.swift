2fb8d4676e0d61920243ba099be5057ba052b101
wangy <wangyunze412@gmail.com>
2020-11-22 20:12:52 +0800

    Cherry pick: Add "convenience" init to Block for easier debugging and mock data
    - Modified it slightly to use no argument label, so Block("Hello")
      instead of Block(name: "Hello")

--- SIS App/Models/CheckInManager.swift
+++ SIS App/Models/CheckInManager.swift
@@ -130,7 +130,7 @@ class CheckInManager: ObservableObject {
                         CheckInSession(
                             checkedIn: Date(timeIntervalSince1970: 1_604_841_082),
                             checkedOut: Date(timeIntervalSince1970: 1_604_841_082 + 3600),
-                            target: Block(name: "Raja Block", location: Location(longitude: 1, latitude: 1), radius: 1)
+                            target: Block("Raja Block")
                         ),
                     ]
                 ),
