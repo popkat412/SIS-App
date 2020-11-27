5231beb4d682e8a52c5717e0494cd8cf3892ff11
wangy <wangyunze412@gmail.com>
2020-11-27 11:10:02 +0800

    Add delay before geofence is triggered (not tested)

--- SIS App/Models/UserLocationManager.swift
+++ SIS App/Models/UserLocationManager.swift
@@ -25,6 +25,9 @@ class UserLocationManager: NSObject, ObservableObject, CLLocationManagerDelegate
 
     let locationManager = CLLocationManager()
 
+    private var isInsideSchool: Bool { userLocation!.distance(from: Constants.schoolLocation) <= Constants.schoolRadius }
+    private var previouslyInsideSchool: Bool { previousUserLocation!.distance(from: Constants.schoolLocation) <= Constants.schoolRadius }
+
     override init() {
         super.init()
 
@@ -48,31 +51,54 @@ class UserLocationManager: NSObject, ObservableObject, CLLocationManagerDelegate
         }
 
         if userLocation != nil, previousUserLocation != nil {
-            let isInsideSchool = userLocation!.distance(from: Constants.schoolLocation) <= Constants.schoolRadius
-            let previouslyInsideSchool = previousUserLocation!.distance(from: Constants.schoolLocation) <= Constants.schoolRadius
-
             if isInsideSchool { // Inside of school
                 for block in DataProvider.getBlocks() {
-                    let currentlyInBlock = isInsideBlock(location: userLocation!, block: block)
-                    let previouslyInBlock = isInsideBlock(location: previousUserLocation!, block: block)
+                    let currentlyInBlock = isCurrentlyInside(block: block)
+                    let previouslyInBlock = isPreviouslyInside(block: block)
                     if currentlyInBlock, !previouslyInBlock {
                         // Entered a block!
                         print("📍 entered a block: \(block.name)")
-                        NotificationCenter.default.post(name: .didEnterBlock, object: nil, userInfo: [Constants.notificationCenterBlockUserInfo: block])
+                        DispatchQueue.main.asyncAfter(deadline: .now() + Constants.geofenceDelayTime) {
+                            if self.isCurrentlyInside(block: block) {
+                                print("📍 still inside \(block.name) after \(Constants.geofenceDelayTime)s")
+                                NotificationCenter.default.post(name: .didEnterBlock, object: nil, userInfo: [Constants.notificationCenterBlockUserInfo: block])
+                            } else {
+                                print("📍 no longer inside \(block.name) after \(Constants.geofenceDelayTime)")
+                            }
+                        }
                     } else if !currentlyInBlock, previouslyInBlock {
                         // Left a block!
                         print("📍 left a block: \(block.name)")
-                        NotificationCenter.default.post(name: .didExitBlock, object: nil, userInfo: [Constants.notificationCenterBlockUserInfo: block])
+                        DispatchQueue.main.asyncAfter(deadline: .now() + Constants.geofenceDelayTime) {
+                            if !self.isCurrentlyInside(block: block) {
+                                print("📍 still outside \(block.name) after \(Constants.geofenceDelayTime)s")
+                                NotificationCenter.default.post(name: .didExitBlock, object: nil, userInfo: [Constants.notificationCenterBlockUserInfo: block])
+                            } else {
+                                print("📍 no longer inside \(block.name) after \(Constants.geofenceDelayTime)")
+                            }
+                        }
                     }
                 }
             }
 
             if isInsideSchool, !previouslyInsideSchool {
                 print("📍 entered the school")
-                NotificationCenter.default.post(name: .didEnterSchool, object: nil)
+                DispatchQueue.main.asyncAfter(deadline: .now() + Constants.geofenceDelayTime) {
+                    print("📍 entered school, \(Constants.geofenceDelayTime) seconds later")
+                    if self.isInsideSchool {
+                        print("📍 still inside school after \(Constants.geofenceDelayTime)s")
+                        NotificationCenter.default.post(name: .didEnterSchool, object: nil)
+                    }
+                }
             } else if !isInsideSchool, previouslyInsideSchool {
                 print("📍 exited the schoool ")
-                NotificationCenter.default.post(name: .didExitSchool, object: nil)
+                DispatchQueue.main.asyncAfter(deadline: .now() + Constants.geofenceDelayTime) {
+                    print("📍 exited school, \(Constants.geofenceDelayTime) seconds later")
+                    if self.isInsideSchool {
+                        print("📍 still outside school after \(Constants.geofenceDelayTime)s")
+                        NotificationCenter.default.post(name: .didExitSchool, object: nil)
+                    }
+                }
             }
         } else {
             print("userlocation or previous user location is nil :(")
@@ -90,7 +116,13 @@ class UserLocationManager: NSObject, ObservableObject, CLLocationManagerDelegate
 
     // MARK: Helper Methods
 
-    private func isInsideBlock(location: CLLocation, block: Block) -> Bool {
-        location.distance(from: block.location.toCLLocation()) <= block.radius
+    private func isCurrentlyInside(block: Block) -> Bool {
+        guard let userLocation = userLocation else { return false }
+        return userLocation.distance(from: block.location.toCLLocation()) <= block.radius
+    }
+
+    private func isPreviouslyInside(block: Block) -> Bool {
+        guard let previousUserLocation = previousUserLocation else { return false }
+        return previousUserLocation.distance(from: block.location.toCLLocation()) <= block.radius
     }
 }
