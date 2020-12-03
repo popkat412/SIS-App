71d27e32154d47b05a875562a7af719ccd5d4471
wangy <wangyunze412@gmail.com>
2020-12-03 15:09:29 +0800

    Refactor LoginView because it got too complicated

--- SIS App/Views/LoginView.swift
+++ SIS App/Views/LoginView.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 import SwiftUIX
 
+private typealias OnErrorCallback = (Error) -> Void
+
 struct LoginView: View {
     @EnvironmentObject var userAuthManager: UserAuthManager
 
@@ -15,6 +17,7 @@ struct LoginView: View {
     @State private var password: String = ""
 
     @State private var showingActivityIndicator = false
+    @State private var showingConfirmPassword = false
 
     @State private var alertItem: AlertItem?
 
@@ -32,83 +35,17 @@ struct LoginView: View {
                 }
                 Spacer()
 
-                VStack {
-                    HStack {
-                        Text("Email: ")
-                        Spacer()
-                    }
-
-                    CocoaTextField("Email", text: $email)
-                        .isFirstResponder(true)
-                        .autocapitalization(.none)
-                        .keyboardType(.emailAddress)
-                        .padding(.horizontal)
-                        .padding(.vertical, 10)
-                        .overlay(
-                            RoundedRectangle(cornerRadius: 8)
-                                .stroke(Color(white: 0.7), lineWidth: 2)
-                        )
-                }
-
-                VStack {
-                    HStack {
-                        Text("Password: ")
-                        Spacer()
-                    }
-
-                    SecureField("Password", text: $password)
-                        .autocapitalization(.none)
-
-                        .padding(.horizontal)
-                        .padding(.vertical, 10)
-                        .overlay(
-                            RoundedRectangle(cornerRadius: 8)
-                                .stroke(Color(white: 0.7), lineWidth: 2)
-                        )
-                }
+                EmailView(email: $email)
+                PasswordView(password: $password)
 
                 Spacer()
                     .frame(height: 50)
 
-                Button(action: {
-                    print("🔥 sign in button pressed")
-//                    guard email.hasSuffix(Constants.riEmailSuffix) else {
-//                        alertItem = MyErrorInfo("Please use your RI email").toAlertItem()
-//                        return
-//                    }
+                VStack {
+                    LoginButton(showingActivityIndicator: $showingActivityIndicator, email: email, password: password, onError: onError)
 
-                    userAuthManager.signIn(email: email, password: password, onError: onError)
-                    showingActivityIndicator = true
-                }) {
-                    Text("Login")
+                    SignupButton(showingActivityIndicator: $showingActivityIndicator, alertItem: $alertItem, email: email, password: password, onError: onError)
                 }
-                .buttonStyle(GradientButtonStyle(gradient: Constants.greenGradient))
-
-                Button(action: {
-                    print("🔥 sign up button pressed")
-//                    guard email.hasSuffix(Constants.riEmailSuffix) else {
-//                        alertItem = MyErrorInfo("Please use your RI email").toAlertItem()
-//                        return
-//                    }
-
-                    userAuthManager.signUp(
-                        email: email,
-                        password: password,
-                        onSentEmailVerfication: {
-                            print("🔥 sent email verification!")
-                            showingActivityIndicator = false
-                            alertItem = AlertItem(
-                                title: "Please verify your account",
-                                message: "A message has been sent to \(userAuthManager.userEmail). Click on the link to verify your account"
-                            )
-                        },
-                        onError: onError
-                    )
-                    showingActivityIndicator = true
-                }) {
-                    Text("Sign up")
-                }
-                .buttonStyle(GradientButtonStyle(gradient: Constants.blueGradient))
 
                 Spacer()
                 Spacer() // slight hack but eh whatever
@@ -138,5 +75,100 @@ struct LoginView_Previews: PreviewProvider {
     static var previews: some View {
         LoginView()
             .environmentObject(UserAuthManager())
+    }
+}
+
+private struct EmailView: View {
+    @Binding var email: String
+
+    var body: some View {
+        VStack {
+            HStack {
+                Text("Email: ")
+                Spacer()
+            }
+
+            CocoaTextField("Email", text: $email)
+                .isFirstResponder(true)
+                .autocapitalization(.none)
+                .keyboardType(.emailAddress)
+                .myTextFieldStyle()
+        }
+    }
+}
+
+private struct PasswordView: View {
+    @Binding var password: String
+
+    var body: some View {
+        VStack {
+            HStack {
+                Text("Password: ")
+                Spacer()
+            }
+
+            SecureField("Password", text: $password)
+                .autocapitalization(.none)
+                .textFieldStyle(MyTextFieldStyle())
+        }
+    }
+}
+
+private struct LoginButton: View {
+    @EnvironmentObject var userAuthManager: UserAuthManager
+    @Binding var showingActivityIndicator: Bool
+    let email, password: String
+    let onError: OnErrorCallback
+
+    var body: some View {
+        Button(action: {
+            print("🔥 sign in button pressed")
+//            guard email.hasSuffix(Constants.riEmailSuffix) else {
+//                alertItem = MyErrorInfo("Please use your RI email").toAlertItem()
+//                return
+//            }
+
+            userAuthManager.signIn(email: email, password: password, onError: onError)
+            showingActivityIndicator = true
+        }) {
+            Text("Login")
+        }
+        .buttonStyle(GradientButtonStyle(gradient: Constants.greenGradient))
+    }
+}
+
+private struct SignupButton: View {
+    @EnvironmentObject var userAuthManager: UserAuthManager
+    @Binding var showingActivityIndicator: Bool
+    @Binding var alertItem: AlertItem?
+    let email, password: String
+    let onError: OnErrorCallback
+
+    var body: some View {
+        Button(action: {
+            print("🔥 sign up button pressed")
+//            guard email.hasSuffix(Constants.riEmailSuffix) else {
+//                alertItem = MyErrorInfo("Please use your RI email").toAlertItem()
+//                return
+//            }
+
+            userAuthManager.signUp(
+                email: email,
+                password: password,
+                onSentEmailVerfication: {
+                    print("🔥 sent email verification!")
+                    showingActivityIndicator = false
+                    alertItem = AlertItem(
+                        title: "Please verify your account",
+                        message: "A message has been sent to \(userAuthManager.userEmail). Click on the link to verify your account"
+                    )
+                },
+                onError: onError
+            )
+            showingActivityIndicator = true
+        }) {
+            Text("Sign up")
+        }
+        .buttonStyle(GradientButtonStyle(gradient: Constants.blueGradient))
     }
 }
