0417075d29397aa0d11c9361b1c84615234112c9
wangy <wangyunze412@gmail.com>
2020-12-11 16:29:14 +0800

    Remove more silly messages

--- SIS App/Models/SessionInvalidError.swift
+++ SIS App/Models/SessionInvalidError.swift
@@ -8,8 +8,8 @@
 import Foundation
 
 enum SessionInvalidError: String, Identifiable {
-    case checkedOutBeforeCheckedIn = "You can't check out before you check in silly boi"
-    case sessionsIntersecting = "You can't be in two places at once, stop trying to break the system"
+    case checkedOutBeforeCheckedIn = "You can't check out before you check in"
+    case sessionsIntersecting = "You can't be in two places at once"
 
     var id: String { rawValue }
 }
