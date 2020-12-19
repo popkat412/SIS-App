e6415bdb81f883b6d2c71e5313cf3c1b267831f5
wangy <wangyunze412@gmail.com>
2020-12-19 18:11:06 +0800

    Combine string extensions into one file

--- SIS App/Utilities/String+HelperExtensions.swift
+++ SIS App/Utilities/String+HelperExtensions.swift
@@ -1,11 +1,17 @@
 //
-//  String+Regex.swift
+//  String+HelperProtocols.swift
 //  SIS App
 //
-//  Created by Wang Yunze on 10/12/20.
+//  Created by Wang Yunze on 3/12/20.
 //
 
 import Foundation
+
+extension String: Identifiable { public var id: String { self }}
+
+extension String: LocalizedError {
+    public var errorDescription: String? { self }
+}
 
 extension String {
     func matches(regex: String) -> Bool {
